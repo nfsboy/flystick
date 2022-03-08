6d300b4d382779e020d5e4bd6e4dd93e84af95a2
nfsboy <nfsboyzxd1125@gmail.com>
2022-03-09 07:31:18 +0800

    saitaike x55 need 2usb joystick

--- flystick_config.py
+++ flystick_config.py
@@ -7,9 +7,7 @@ bonus - it had just the throttle lever I wanted.
 from flystick_conf_models import *
 
 stick = Joystick(0)
-
-# aileron trim, hat side-to-side axis
-ail_trim = stick.hat_switch(hat=0, axis=0, positions=41, initial=20)
+stick = joystick(1)
 
 # Raspberry Pi GPIO pin where to output the PPM signal.
 # Pin map: http://wiki.mchobby.be/images/3/31/RASP-PIZERO-Correspondance-GPIO.jpg
@@ -19,7 +17,7 @@ PPM_OUTPUT_PIN = 18
 # Output (PPM) channels.
 CHANNELS = (
     # channel 1: aileron with trim
-    stick.axis(0) + ail_trim * 0.5,
+    stick.axis(0) ,
     # a more elaborate example with reverse, offset, weight and trim:
     #(-stick.axis(0) + 0.1) * 0.7 + ail_trim * 0.5,
     # channel 2: elevator (reversed)
@@ -36,25 +34,3 @@ CHANNELS = (
     stick.button(3),
 )
 
-# dual-channel display component
-stick_dot = XYDot(col=5)
-
-# Render outputs (channels). One-to-one line match to CHANNELS.
-DISPLAY = (
-    # channel 1: dot horizontal axis
-    stick_dot.horizontal(),
-    # channel 2: dot vertical axis
-    stick_dot.vertical(),
-    # channel 3: throttle bar
-    YBar(col=0, width=2),
-    # channel 4: flight mode switch
-    YDot(col=9),
-    # channels 5-8: buttons demo
-    Block(corner=(10, 0)),
-    Block(corner=(10, 1)),
-    Block(corner=(10, 2)),
-    Block(corner=(10, 3)),
-)
-
-# TODO what's the range? 128? http://www.issi.com/WW/pdf/31FL3730.pdf
-DISPLAY_BRIGHTNESS = 10
